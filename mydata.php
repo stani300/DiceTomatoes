12c2a37460e8670ccdaf8c34839a45c84f04f694
stani300 <sandro.tanis@gmail.com>
2019-02-26 22:22:55 -0500

    Update mydata.php
    fixed host

--- mydata.php
+++ mydata.php
@@ -16,50 +16,49 @@
 	</style>
 </head>
 <body>
-<h1>Display Data from DB</h1>
+	<h1>Display Data from DB</h1>
 
-<?php
+	<?php
 
-$servername = "localhost";
-$username = "root";
-$password = "database";
-$database = 'mydata';
+	$servername = "127.0.0.1";
+	$username = "root";
+	$password = "database";
+	$database = 'mydata';
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $database);
 
-if (!$conn) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-
-// Check connection
-echo "Connected successfully";
-
-#include('connect -mysql.php');
-
-$query = "SELECT * FROM mydata";
-$query_result = mysqli_query($conn, $query) or die ('error getting');
-
-echo "<table>";
-echo "<tr><th>ID</th><th>FIRST NAME</th><th>LAST NAME</th></tr>";
-
-while($row = mysqli_fetch_array($query_result, MYSQL_ASSOC)) {
-	echo "<tr><td>";
-	echo $row ['peopleid'];
-	echo "</td><td>";
-	echo $row ['firstname'];
-	echo "</td><td>";
-	echo $row ['lastname'];
-	echo "</td><tr>";
-}
-echo "</table>";
+	if (!$conn) {
+	    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+	    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+	    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+	    exit;
+	}
 
 
-?>
+	// Check connection
+	echo "Connected successfully";
 
-</body>
+	#include('connect -mysql.php');
+ 
+	$query = "SELECT * FROM mydata";
+	$query_result = mysqli_query($conn, $query) or die ('error getting');
+
+	echo "<table>";
+	echo "<tr><th>ID</th><th>FIRST NAME</th><th>LAST NAME</th></tr>";
+
+	while($row = mysqli_fetch_array($query_result, MYSQL_ASSOC)) {
+		echo "<tr><td>";
+		echo $row['ID'];
+		echo "</td><td>";
+		echo $row['FIRSTNAME'];
+		echo "</td><td>";
+		echo $row['LASTNAME'];
+		echo "</td><tr>";
+	}
+	echo "</table>";
+
+	?>
+
+	</body>
 </html>
