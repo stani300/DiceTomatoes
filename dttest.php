b0bf29d57bfc96f62267e51c3dacf929887677a7
John Willoughby <john@savix.net>
2019-03-17 15:29:55 -0400

    x

--- dttest.php
+++ dttest.php
@@ -1,18 +1,21 @@
 <?php
 
+echo ( "start" );
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $database);
+
+	echo ( "connection made" );
 
 	if (!$conn) {
     echo ( "Error: Unable to connect to MySQL: errno = " . mysqli_connect_errno() . ", error text = " .  mysqli_connect_error() );
 	  exit;
 	};
 
-			$query = "SELECT * FROM movies";
-			$query_result = mysqli_query($conn, $query);
+	$query = "SELECT * FROM movies";
+	$query_result = mysqli_query($conn, $query);
 
-			while($row = $query_result->fetch_array(MYSQLI_ASSOC) {
-				echo ( "name " . $row['title']);
-			}
+	while($row = $query_result->fetch_array(MYSQLI_ASSOC) {
+		echo ( "name " . $row['title']);
+	}
 
 ?>
