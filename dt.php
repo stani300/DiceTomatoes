dd4c235187c12da68810e9010f3e2a120f8d447a
John Willoughby <john@savix.net>
2019-03-18 06:52:11 -0400

    x

--- dt.php
+++ dt.php
@@ -44,9 +44,9 @@
 			while( ($row = $query_result->fetch_array(MYSQLI_ASSOC) ) && ( $cnt++ < 27)  ) {
 			   $sdat[$cnt]->name = $row['title'];
 			   // $sdat[$cnt]->year = $row['year'];
-				 $sdat[$cnt]->year = 1950+rand(0,69) ;
+	//			 $sdat[$cnt]->year = 1950+rand(0,69) ;
 				 // $sdat[$cnt++]->rating = $row['rating'];
-			   $sdat[$cnt]->rating = rand(1,10);
+	//		   $sdat[$cnt]->rating = rand(1,10);
 			}
 			break;
 		case "analytics":
@@ -68,10 +68,10 @@
 			// now go to the db and find the data
 
 			// or fake it for now with random values
-			for ( i=1; i<6; i++ ) {
-				$sdat[i]->val = rand(5,20);
-				$sdat[i]->name = 2000 + i;
-			}
+//			for ( i=1; i<6; i++ ) {
+//				$sdat[i]->val = rand(5,20);
+//				$sdat[i]->name = 2000 + i;
+//			}
 
 			break;
 		default:
