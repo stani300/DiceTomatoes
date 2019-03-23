8392e0d2f1cd87c4fcb599ab86a4a8a267d4f8b6
doniarobinson <doniamae@gmail.com>
2019-03-23 10:03:09 -0500

    Change search query

--- dt.php
+++ dt.php
@@ -49,7 +49,7 @@
 			 	//$sdat[$cnt]->year = 1950+rand(0,69) ;
 				
 				// this will need to be average of ratings from ratings				
-				$sdat[$cnt++]->rating = $row['avg'];
+				//$sdat[$cnt++]->rating = $row['avg'];
 		   	//$sdat[$cnt]->rating = rand(1,10);
 			}
 			break;
