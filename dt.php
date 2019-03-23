da37dd9fb80914745b5fdbde75775d276a32df32
doniarobinson <doniamae@gmail.com>
2019-03-23 12:12:39 -0500

    Modify browse query

--- dt.php
+++ dt.php
@@ -53,7 +53,7 @@
 			 	//$sdat[$cnt]->year = 1950+rand(0,69) ;
 				
 				// this will need to be average of ratings from ratings				
-				$sdat[$cnt++]->rating = $row['avg_score'];
+				$sdat[$cnt]->rating = $row['avg_score'];
 		   	//$sdat[$cnt]->rating = rand(1,10);
 			}
 			break;
