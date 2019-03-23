87db863900a99b453d1f22a235ec91d4d283f9fc
doniarobinson <doniamae@gmail.com>
2019-03-23 12:14:24 -0500

    Browse query is showing correct year and average rating

--- dt.php
+++ dt.php
@@ -33,14 +33,12 @@
 
 			// find movies that match the search string in target
 			$query = "SELECT m.*, AVG(r.rating) AS avg_score FROM movies AS m JOIN ratings AS r ON r.movie_id=m.id WHERE title LIKE '" . $target . "' GROUP BY r.movie_id";
-			//$query = "SELECT m.*, AVG(r.rating) AS avg_score FROM movies AS m JOIN ratings AS r ON r.movie_id=m.id GROUP BY r.movie_id";
 
 			//$query = "SELECT * FROM movies WHERE title LIKE '".$target."' ";
 
 			$query_result = mysqli_query($conn, $query);
 
 			$cnt = 0;
-			$sdat[$cnt]->name = $query;
 			// burn first row because it's titles
 			// NO, IT'S NOT
 			//$row = $query_result->fetch_array(MYSQLI_ASSOC);
