dcace857c77732fbd1a8c4d2c05045da5f43a8b3
John Willoughby <john@savix.net>
2019-04-07 17:49:45 -0400

    x

--- rate.php
+++ rate.php
@@ -23,45 +23,74 @@ uid = 1;
 			<div class="col-sm-12">
 				<h1>Rate</h1>
 
-				<?php
-						if (!isset( $_SESSION['logged_in'])) {
 
-							if (!isset( $_SESSION['logged_in'])) {
-								echo "<p class='redtxt'>" . $_SESSION['err'] . "</p>";
-								unset($_SESSION['err']);
-							}
-					?>
-					
-							<p>You must be logged in to view this page.</p>
-							
-							<form action="login.php" method="post">
 
-							<input type="hidden" name="location" value="rate.php">
 
-							<div class="form-group">
-								<label class="mylabel-box">Username:</label>
-								<input type="text" name="username" required id="user" class="myform-control" />
-							</div>
+        					<?php
+        						if (!isset( $_SESSION['logged_in'])) {
 
-							<div class="form-group">
-								<label class="mylabel-box">Password:</label>
-								<input type="password" name="password" required id="pwd" class="myform-control" />
-							</div>
+        							if (!isset( $_SESSION['logged_in'])) {
+        								echo "<p class='redtxt'>" . $_SESSION['err'] . "</p>";
+        								unset($_SESSION['err']);
+        							}
+        					?>
 
-							<div class="form-group">
-								<input type="submit" id="LEnter" class="btn btn-primary" value="Submit">
-							</div>
-							</form>
+        							<p>You must be logged in to view this page.</p>
 
-					<?php
-						}
+        							<form action="login.php" method="post">
 
-						else {
-					?>
+        							<input type="hidden" name="location" value="recommendations.php">
 
-				<p>You are logged in as user: <strong><span id="currUser"></span></strong></p>
-				<p><a href="logout.php">Log out</a></p>
-				<br />
+        							<div class="form-group">
+        								<label class="mylabel-box">Username:</label>
+        								<input type="text" name="username" required id="user" class="myform-control" />
+        							</div>
+
+        							<div class="form-group">
+        								<label class="mylabel-box">Password:</label>
+        								<input type="password" name="password" required id="pwd" class="myform-control" />
+        							</div>
+
+        							<div class="form-group">
+        								<input type="submit" id="LEnter" class="btn btn-primary" value="Submit">
+        							</div>
+        							</form>
+
+        					<?php
+        						}
+
+        						else {
+        					?>
+        							<p>You are logged in as user: <strong><?php echo $_SESSION['critic_name']; ?></strong></p>
+        							<p><a href="logout.php">Log out</a></p>
+        							<br />
+
+        							<p>Can't to decide what to watch? Let the movie recommendation algorithm suggest some movies you might like! Simply select a genre, and based on the past ratings you've submitted, it will recommend movies it thinks you might like!</p>
+
+        							<?php
+        								$servername = "127.0.0.1";
+        								$username = "root";
+        								$password = "UIUC411";
+        								$database = 'diced_tomatoes';
+
+        								// Create connection
+        								$conn = mysqli_connect($servername, $username, $password, $database);
+
+        								if (!$conn) {
+        									$sdat[0]->msg = "Error: Unable to connect to MySQL: errno = " . mysqli_connect_errno() . ", error text = " .  mysqli_connect_error();
+        									$sdat[0]->err = 1;
+        									$jrtn = json_encode($sdat);
+        									echo $jrtn;
+        									exit;
+        								};
+
+        								$query0 = "SELECT * FROM genre ORDER BY genre_name";
+          							$query_result0 = mysqli_query($conn, $query0);
+
+        								?>
+
+
+
 
 				<p><strong>Your existing ratings:</strong></p>
 
