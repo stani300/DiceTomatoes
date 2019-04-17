8d331acafd73e10c20019fa65d6b87c6ca4c130f
John Willoughby <john@savix.net>
2019-04-17 19:48:28 -0400

    x

--- dt.php
+++ dt.php
@@ -29,7 +29,7 @@
 			$avgYr = $params->{'avgYr'};
 
 			// *** This is just a placeholder to show the screens - it just returns movies that include the string "cari"
-			$query = "SELECT m.*, AVG(r.rating) AS avg_score FROM movies AS m JOIN ratings AS r ON r.movie_id=m.id WHERE SUBSTR(m.release_date,1,4) LIKE " . $avgYr ;
+			$query = "SELECT m.*, AVG(r.rating) AS avg_score FROM movies AS m JOIN ratings AS r ON r.movie_id=m.id WHERE SUBSTR(m.release_date,1,4) LIKE '" . $avgYr . "'" ;
 
 			$query_result = mysqli_query($conn, $query);
 
