1a0c0ffc2d417ef0fc3e71a5c824e87addc92895
John Willoughby <john@savix.net>
2019-04-19 05:36:03 -0400

    x

--- ptest.php
+++ ptest.php
@@ -14,6 +14,15 @@ $database = 'diced_tomatoes';
 $sdat[0] = new stdClass();
 $sdat[0]->err=0;
 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+  $sdat[0]->errmsg = "Error: Unable to connect to MySQL: errno = " . mysqli_connect_errno() . ", error text = " .  mysqli_connect_error();
+  $sdat[0]->err = 1;
+  $action = "fail";
+};
+
 echo "test"; exit ("xyzzy");
 
 ?>
