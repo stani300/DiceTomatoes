defc46cd6551f1b520b6369f3a9f5c5a99a613c9
John Willoughby <john@savix.net>
2019-04-19 08:08:23 -0400

    x

--- dt.php
+++ dt.php
@@ -42,7 +42,8 @@
 				$avgScore = $row['avg_score'];
 				$thisLang = $row['language'];
 				$thisRun = $row['runtime'];
-				if ( ( $thisYr > $avgYr - 5 ) && ( $thisYr < $avgYr + 5 ) && ( $thisLang == $mylang ) && ( $avgScore > 4 ) && ( $thisRun > ($avgRun + 10) ) ) {
+				$sdat[0]->avgRun = $avgRun;
+				if ( ( $thisYr > $avgYr - 5 ) && ( $thisYr < $avgYr + 5 ) && ( $thisLang == $mylang ) && ( $avgScore > 4 ) && ( (int) $thisRun > (int) ($avgRun + 10) ) ) {
 					$sdat[$cnt] = new stdClass();
 					$sdat[$cnt]->id = $row['id'];
 					$sdat[$cnt]->name = $row['title'];
