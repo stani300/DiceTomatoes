558c4ecd0ffdfa36b7d21ffa3e29c68ece63bee7
John Willoughby <john@savix.net>
2019-04-19 05:34:55 -0400

    x

--- ptest.php
+++ ptest.php
@@ -1,5 +1,19 @@
 <?php
 
+$str_json = file_get_contents('php://input');
+$params = json_decode ( $str_json );
+
+$action = $params->{'action'};
+$target = $params->{'target'};
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "UIUC411";
+$database = 'diced_tomatoes';
+
+$sdat[0] = new stdClass();
+$sdat[0]->err=0;
+
 echo "test"; exit ("xyzzy");
 
 ?>
